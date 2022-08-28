808ceccb9d15b8f93fda15013749896e28b7918f
huangjz <1249293339@qq.com>
2022-08-28 23:07:39 +0800

    feat: 微信登录

--- store/index.js
+++ store/index.js
@@ -1,13 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import search from './modules/search'
+import user from './modules/user'
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
 	// 模块
 	modules: {
-		search
+		search,
+		user
 	}
 })
 
